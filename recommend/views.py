f6c907c1671249fef1b6751fe28175a07abab685
150202144 <150202144@kocaeli.edu.tr>
2021-05-24 12:14:46 +0300

    view.py mert

--- recommend/views.py
+++ recommend/views.py
@@ -13,6 +13,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 #samedin degisimi
 
+# mertin muhteşem değişimi
+
 # Create your views here.
 
 def index(request):
