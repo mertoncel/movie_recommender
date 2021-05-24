1cf8a5eb386ca775fd608a01bce0ef1cf047cc93
Samed MUSABAŞOĞLU <samedmusabasoglu@gmail.com>
2021-05-24 07:39:47 +0300

    Descriptionlu

--- recommend/models.py
+++ recommend/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import User
 class Movie(models.Model):
     title = models.CharField(max_length=200)
     genre = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000, default="")
     movie_logo = models.FileField()
 
     def __str__(self):
